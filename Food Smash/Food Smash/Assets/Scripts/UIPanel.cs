80239bc27120777385dd4ae24d9aca860ae84db6
scartt <scartt@bu.edu>
2021-01-10 02:10:37 +0800

    add panel and character to battle system

---
+++ Food Smash/Food Smash/Assets/Scripts/UIPanel.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPanel : MonoBehaviour
+{
+    public RawImage charaAImg;
+    public RawImage charaBImg;
+    public UIPanel contentBg;
+    public Text contentTxt;
+    public Canvas canvas;
+
+    public void ShowCanvas(bool value)
+    {
+        canvas.enabled = value;
+    }
+
+    public void ShowCharaA(bool value)
+    {
+        charaAImg.enabled = value;
+    }
+
+    public void ShowCharaB(bool value)
+    {
+        charaBImg.enabled = value;
+    }
+
+    public void ShowContentBg(bool value)
+    {
+        contentBg.enabled = value;
+    }
+
+    public void ShowContentText(bool value)
+    {
+        contentTxt.enabled = value;
+    }
+
+    public void SetContentText(string value)
+    {
+        contentTxt.text = value;
+    }
+
+    public void ChangeCharaATex(Texture tex)
+    {
+        charaAImg.texture = tex;
+    }
+
+    public void ChangeCharaBTex(Texture tex)
+    {
+        charaBImg.texture = tex;
+    }
+}
---
+++ Food Smash/Food Smash/Assets/Scripts/camera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    private float yourDesiredSize = 32f;
+
+    void Awake()
+    {
+        Camera.main.orthographicSize = yourDesiredSize;
+    }
+}
